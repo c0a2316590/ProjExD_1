dbf6db518748ce1ee4bda1ae8a6126258cdd34e8
c0a23165 <c0a2316590@edu.teu.ac.jp>
2024-11-19 16:11:25 +0900

    練習７：こうかとんワープ修正

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,19 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    flip_img = pg.transform.flip(bg_img, True, False)
     koukaton = pg.image.load("fig/3.png")
     koukaton = pg.transform.flip(koukaton, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        if tmr >= 1600 - 800:
-            tmr = 0
-        screen.blit(bg_img, [-tmr, 0]) #screen Surfaceに背景画像を張り付ける
+        
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0]) #screen Surfaceに背景画像を張り付ける
+        screen.blit(flip_img, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
+        screen.blit(flip_img, [4800-x, 200])
         screen.blit(koukaton, [300, 200])
         pg.display.update()
         tmr += 1        
